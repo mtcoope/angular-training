8b706771b2c8508ba1be1d27de9006cb7db86ef3
michael cooper <mtc3108@gmail.com>
2020-01-11 15:48:19 -0500

    wip

---
+++ angular-training/src/app/recipes/recipe.service.ts
@@ -0,0 +1,14 @@
+import { Recipe } from './recipe.model';
+import { EventEmitter } from '@angular/core';
+
+export class RecipeService {
+    recipeSelected = new EventEmitter<Recipe>();
+    private recipes: Recipe[] = [
+        new Recipe('A Test Recipe', 'This is a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
+        new Recipe('A Test Recipe2', 'This is a test2', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
+      ];
+
+      getRecipes() {
+          return this.recipes.slice();
+      }
+}
